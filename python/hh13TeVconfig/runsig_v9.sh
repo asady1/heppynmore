f20666f0d198901c7b188071c9727cdaab40a24b
Alice Sady <asady1@jhu.edu>
2016-01-21 13:30:56 -0600

    updating analysis selection

---
+++ python/hh13TeVconfig/runsig_v9.sh
@@ -0,0 +1,17 @@
+#/bin/sh
+
+#Rad 1000
+python generalTreeAnalyzer_v9_pt.py --pathIn=/eos/uscms/store/group/lpchbb/HeppyNtuples/V14/BulkGravTohhTohbbhbb_narrow_M-1000_13TeV-madgraph/VHBB_HEPPY_V14_BulkGravTohhTohbbhbb_narrow_M-1000_13TeV-madgraph__RunIISpring15MiniAODv2-74X_mcRun2_asymptotic_v2-v1/151024_223151/0000/ --outName=BG_1_v9_pt --min=0 --max=2 --file=TxtFiles/BG1000.txt &
+
+#Rad 1200
+python generalTreeAnalyzer_v9_pt.py --pathIn=/eos/uscms/store/group/lpchbb/HeppyNtuples/V14/BulkGravTohhTohbbhbb_narrow_M-1200_13TeV-madgraph/VHBB_HEPPY_V14_BulkGravTohhTohbbhbb_narrow_M-1200_13TeV-madgraph__RunIISpring15MiniAODv2-74X_mcRun2_asymptotic_v2-v1/151025_082927/0000/ --outName=BG_1p2_v9_pt --min=0 --max=1 --file=TxtFiles/BG1200.txt &
+
+#Rad 1600
+python generalTreeAnalyzer_v9_pt.py --pathIn=/eos/uscms/store/group/lpchbb/HeppyNtuples/V14/BulkGravTohhTohbbhbb_narrow_M-1600_13TeV-madgraph/VHBB_HEPPY_V14_BulkGravTohhTohbbhbb_narrow_M-1600_13TeV-madgraph__RunIISpring15MiniAODv2-74X_mcRun2_asymptotic_v2-v1/151025_084646/0000/ --outName=BG_1p6_v9_pt --min=0 --max=1 --file=TxtFiles/BG1600.txt &
+
+#Rad 2000
+python generalTreeAnalyzer_v9_pt.py --pathIn=/eos/uscms/store/group/lpchbb/HeppyNtuples/V14/BulkGravTohhTohbbhbb_narrow_M-2000_13TeV-madgraph/VHBB_HEPPY_V14_BulkGravTohhTohbbhbb_narrow_M-2000_13TeV-madgraph__RunIISpring15MiniAODv2-74X_mcRun2_asymptotic_v2-v1/151025_083027/0000/ --outName=BG_2_v9_pt --min=0 --max=1 --file=TxtFiles/BG2000.txt &
+
+#Rad 3000
+python generalTreeAnalyzer_v9_pt.py --pathIn=/eos/uscms/store/group/lpchbb/HeppyNtuples/V14/BulkGravTohhTohbbhbb_narrow_M-3000_13TeV-madgraph/VHBB_HEPPY_V14_BulkGravTohhTohbbhbb_narrow_M-3000_13TeV-madgraph__RunIISpring15MiniAODv2-74X_mcRun2_asymptotic_v2-v1/151025_084709/0000/ --outName=BG_3_v9_pt --min=0 --max=1 --file=TxtFiles/BG3000.txt &
+
